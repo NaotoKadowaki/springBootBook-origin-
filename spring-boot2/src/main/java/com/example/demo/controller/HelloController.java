c086bc8f0a2cfc23aba8118eff9b289abc980994
NaotoKadowaki <vivi0601naoto@icloud.com>
2020-11-03 15:15:38 +0900

    boot2 Get値の方法

--- spring-boot2/src/main/java/com/example/demo/controller/HelloController.java
+++ spring-boot2/src/main/java/com/example/demo/controller/HelloController.java
@@ -1,17 +1,26 @@
+
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 
 public class HelloController {
 
-	@RequestMapping("/")
-	
-	
-	public String index() {
-		
-		 return"hello";
+	@GetMapping("/")
+	public String greeting() {
+
+		return "hello";
+
 	}
+
+	@GetMapping("/greeting")
+	public String greeting(@RequestParam(value = "message") String message, Model model) {
+		model.addAttribute("sample", message);
+		return "hello";
+	}
+
 }
