0253ddf6294cdec993f4dc003daf4b655964f5ec
NaotoKadowaki <vivi0601naoto@icloud.com>
2020-11-14 18:03:20 +0900

    無理ゲー

--- spring-security2/src/main/java/com/example/demo/SpringSecurity2Application.java
+++ spring-security2/src/main/java/com/example/demo/SpringSecurity2Application.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringSecurity2Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringSecurity2Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurity2Application.class, args);
+    }
 
 }
