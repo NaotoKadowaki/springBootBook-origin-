587721387f4185214bf1b80c17212f7fcd88182d
NaotoKadowaki <vivi0601naoto@icloud.com>
2021-02-13 12:04:25 +0900

    no message

---
+++ spring-security1jsfile-1nnnnnn-9/src/main/java/com/example/demo/SpringSecurity1jsfile1nnnnnn9Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurity1jsfile1nnnnnn9Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurity1jsfile1nnnnnn9Application.class, args);
+	}
+
+}
--- spring-security2/src/main/java/com/example/demo/repository/SiteUserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.model.SiteUser;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SiteUserRepository extends JpaRepository<SiteUser,Long> {
-    SiteUser findByUsername(String username);
-    boolean existsByUsername(String username);
-}
